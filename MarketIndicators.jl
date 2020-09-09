ba6abbcb3249ad22bc4655f731ce1dd6b7110b7f
Uki D. Lucas <UkiDLucas@gmail.com>
2020-09-08 22:08:17 -0400

    unemployment plots

--- MarketIndicators.jl
+++ MarketIndicators.jl
@@ -16,23 +16,21 @@ end
 
 file_ISM_Mfc_Emp = "data/united-states.ism-manufacturing-employment.csv"
 file_Markkit_Mfc_PMI = "data/united-states.markit-manufacturing-pmi.csv"
-
-println("Functions available:")
-println("df = fetch_data(file_path, date_format=\"yyyy-mm-dd\" )")
-println("df = shift_dates_by_days!(df, shift_days=-1, date_column=1)")
+file_initial_jobless_claims = "data/united-states.initial-jobless-claims.csv"
 
 function fetch_data(file_path, date_format="yyyy-mm-dd")
     isfile(file_path) || # does file exist locally?
-        download( string("https://raw.githubusercontent.com/UkiDLucas/MarketIndicators.jl/master/", file_path), # URL
+        download( string("https://raw.githubusercontent.com/UkiDLucas/MarketIndicators.jl/master/", 
+            file_path), # URL
         file_path) # save it as name
-    
-
     
     return CSV.read(        # returns DataFrame
         file_path, 
         dateformat="$date_format"
         )
 end
+
+println("function: df = fetch_data(file_path, date_format=\"yyyy-mm-dd\" )")
 
 # TEST
 #df = fetch_data(file_ISM_Mfc_Emp, "yyyy.mm.dd")
@@ -50,6 +48,7 @@ function shift_dates_by_days!(df::DataFrame, shift_days::Int64=-1, date_column::
 end
 
 # df = shift_dates_by_days!(df3, -90)
+println("function: df = shift_dates_by_days!(df, shift_days=-1, date_column=1)")
 
 first_date = Date("Jan. 1, 1900", "u. dd, yyyy")
 
@@ -62,6 +61,8 @@ function days_since_day_one(df, row, date_column)
     return rata_die
 end
 
+println("function: rata_die = days_since_day_one(df, row, date_column) ")
+
 function update_rata_die!(df, days_column=1, date_column=2)
     rows = size(df)[1] # first part of the returned tupple
 
@@ -73,10 +74,14 @@ function update_rata_die!(df, days_column=1, date_column=2)
     return df
 end
 
+println("function: df = update_rata_die!(df, days_column=1, date_column=2)")
+
 function insert_rata_die_column!(df)
     rows = size(df)[1] # first number of the tuple
     # https://stackoverflow.com/a/63731422/6312771
     insertcols!(df, 1,  :Day => 1:rows, makeunique=true) # insert as column 1, populate with 1,2,3,..
+    return df
 end
+println("function: df = insert_rata_die_column!(df)")
 
 
