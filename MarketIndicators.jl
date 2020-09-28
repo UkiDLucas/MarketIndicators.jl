a63bb64a06997df1db9209218aad00f03f358a36
Uki D. Lucas <UkiDLucas@gmail.com>
2020-09-28 18:29:22 -0400

    Cleaning DJIA

--- MarketIndicators.jl
+++ MarketIndicators.jl
@@ -91,12 +91,6 @@ end
 
 
 function preview_data(df)
-    println("
-    Usage:
-          preview_data(df)
-          preview_data(df[1:2,1:3])
-    ")
-    
     all_rows = size(df)[1]
     show_rows = 20
     
@@ -107,6 +101,12 @@ function preview_data(df)
     interval = convert(Int64, round(all_rows/show_rows, digits=0))
     rows = collect(1:interval:all_rows)
     show(df[ rows, :], allcols=true) # do NOT limit number of columns if more than 6
+end
+if show_help
+    println("usage:
+                   preview_data(df)
+                   preview_data(df[1:2,1:3])
+    ")
 end
 
 
