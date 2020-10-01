5b52251e26929c184d9de86d4716bd6e6dec5187
Uki D. Lucas <UkiDLucas@gmail.com>
2020-09-30 21:13:43 -0400

    format_dates can take array of strings, or array of Dates

--- MarketIndicators.jl
+++ MarketIndicators.jl
@@ -122,14 +122,19 @@ end
 
 
 using Dates
-function format_dates(dates::Array{String,1}, date_format="yy/mdd")
+function format_dates(dates, date_format="yy/mdd")
     columns = length(dates)
     results = Array{String, 1}(undef, columns) # define array to hold x-axis values
 
     for i in 1:columns
-        results[i] = string( Dates.format(Date(dates[i]), date_format)  ) 
+        if typeof(dates[i]) == Date
+            results[i] = convert(String, Dates.format( dates[i], date_format ) )
+        end
+        if typeof(dates[i]) == String
+            results[i] = convert( String,  Dates.format( Date(dates[i]), date_format ) )
+        end 
     end
-    
+
     return results
 end
 if show_help
