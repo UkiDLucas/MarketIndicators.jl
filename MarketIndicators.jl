8498c22bbb63b018280172241ed3c743d671bca4
Uki D. Lucas <UkiDLucas@gmail.com>
2020-10-07 07:31:48 -0400

    Extracted calculate_average function

--- MarketIndicators.jl
+++ MarketIndicators.jl
@@ -322,3 +322,17 @@ end
 println("")
 
 
+using DataFrames, Statistics
+
+function calculate_average(df::DataFrame, average_days=90, value_column=3)
+    record_count = size(df)[1]
+    averages = zeros(Float64, record_count) # empty array
+    
+    for i in average_days:record_count
+        day_this = df[i,1] # rata die 
+        previous_interval_values = df[i-average_days+1:i, value_column]
+        avg = mean(previous_interval_values)
+        averages[i] = avg
+    end
+    return averages
+end
