833c154604649361c6b8799bed6a471cec030d59
Uki D. Lucas <UkiDLucas@gmail.com>
2020-10-18 13:46:46 -0400

    fixed update_yahoo_finance

--- MarketIndicators.jl
+++ MarketIndicators.jl
@@ -359,13 +359,13 @@ end
 
 
 
-
+using Printf
 function update_yahoo_finance(uri::String, interval = "1d")
     str_interval = "&interval=" * interval
     str_events = "&events=history"
     unix_date_now = @sprintf("%.0f", Dates.datetime2unix(Dates.now()))
     cut_position = findfirst("period2=", uri)[end] # take only second element of the tuple
     str_begining = uri[1:cut_position]
-    uri = begining * unix_date_now * ending
+    uri = str_begining * unix_date_now * str_interval * str_events
     return uri
 end
