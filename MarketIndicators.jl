fea92ed53df2641f19584942460cd862969575e2
Uki D. Lucas <UkiDLucas@gmail.com>
2020-11-06 19:35:22 -0500

    column_to_average

--- MarketIndicators.jl
+++ MarketIndicators.jl
@@ -247,13 +247,13 @@ println("")
 
 using DataFrames, Statistics
 
-function calculate_average(df::DataFrame, average_days=90, value_column=3)
+function calculate_average(df::DataFrame, average_days=90, column_to_average=3)
     record_count = size(df)[1]
     averages = zeros(Float64, record_count) # empty array
     
     for i in average_days:record_count
         day_this = df[i,1] # rata die 
-        previous_interval_values = df[i-average_days+1:i, value_column]
+        previous_interval_values = df[i-average_days+1:i, column_to_average]
         avg = round( mean(previous_interval_values) , digits=2) 
         averages[i] = avg
     end
