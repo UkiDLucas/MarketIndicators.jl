a68ab815b75497e8bfcc01425d61f46352acf9d8
UkiDLucas <UkiDLucas@gmail.com>
2020-11-29 13:54:07 -0500

    added all functions

--- src/functions.jl
+++ src/functions.jl
@@ -11,4 +11,23 @@ using DataFrames
 using CSV, DataFrames
 using Statistics 
 
+
+include("function_add_overal_mean.jl")
+include("function_add_rata_die_column.jl")
+include("function_available_datasets.jl")
+include("function_calculate_average.jl")
+include("function_fetch_data.jl")
+include("function_fetch_dataset.jl")
+include("function_format_dates.jl")
+include("function_populate_missing_dates.jl")
+include("function_position_on_date.jl")
+include("function_preview_data.jl")
+include("function_print_colunms.jl")
+include("function_quantize_column.jl")
+include("function_save_dataset.jl")
+include("function_shift_dates_by_days.jl")
+include("function_update_rata_die.jl")
+include("function_update_yahoo_finance.jl")
+include("function_updated.jl")
+
 println() # prevents the last function from printing
