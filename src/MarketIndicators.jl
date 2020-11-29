d35b3d739c8dc0c87c52df0684960070573bf8ec
UkiDLucas <UkiDLucas@gmail.com>
2020-11-29 11:30:50 -0500

    major re-organization of the structure

---
+++ src/MarketIndicators.jl
@@ -0,0 +1,8 @@
+module MarketIndicators                         # inside this file, declare the module name
+    #import Base.ctranspose                     # and import other packages
+    #export derivative, check_gradient,         # export some of the functions defined in this package
+
+    #include("derivative.jl")                   # include the contents of other files in the module
+    #include("check_derivative.jl")             
+    #include("integrate.jl")
+end                                             # end of module
