72c5c0ab18c90389ee721e970ef984b3166e381b
UkiDLucas <UkiDLucas@gmail.com>
2020-12-05 07:01:15 -0500

    changed Day to Rata_Die, changed hard-coded record_count

--- src/Julia/function_find_day.jl
+++ src/Julia/function_find_day.jl
@@ -1,9 +1,13 @@
 
 function find_day(df::DataFrame, day::Int64)
     #found_items = df[df.Day .== uber_min_day, 1] # Array{Float64,1}
-    bit_array_01 = ( df.Day .== day )
     
-    for id in 1:13066
+    ## identify particular day in DataFrame
+    bit_array_01 = ( df.Rata_Die .== day )
+    
+    record_count = size(df)[1]
+
+    for id in 1:record_count
         if bit_array_01[id] == true
             #println(id, " ", bit_array_01[id])
             return id
