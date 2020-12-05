7dc1f52ce16f6ec1fd24f492733c3db98c73ba2d
UkiDLucas <UkiDLucas@gmail.com>
2020-12-05 08:29:58 -0500

    improved function, added last row, quotes

--- src/Julia/function_preview_data.jl
+++ src/Julia/function_preview_data.jl
@@ -1,19 +1,25 @@
 function preview_data(df, show_rows = 6)
-    all_rows = size(df)[1]
+    show_rows = show_rows-1 # adding the last record later on
+    record_count = size(df)[1]
     
-    if all_rows < show_rows
-        show_rows = all_rows
+    if record_count < show_rows
+        show_rows = record_count
     end
     
-    interval = convert(Int64, round(all_rows/show_rows, digits=0))
-    rows = collect(1:interval:all_rows)
+    interval = convert(Int64, round(record_count/show_rows, digits=0))
+
+    rows = collect(1:interval:record_count) # results in an array of record IDs
+    rows = append!(rows, record_count) # add last record
+
+    println("Showing record IDs ", rows, " at interval ", interval )
+
     show(df[ rows, :], allcols=true) # do NOT limit number of columns if more than 6
 
     println("\n Dataset dimentions: ", size(df) )
-    println("\n variable columns:")
+    println("\n Column names (note any extra spaces):")
     columns = names(df)
     for i in 1:length(columns)
-        println(i, " ", columns[i])
+        println(i, " '", columns[i], "' ") # quotes to show any empty spaces
     end
     return columns
 end
