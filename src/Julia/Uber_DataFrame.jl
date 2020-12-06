48b3642097c19f3ba8c4d06d71d21fd5e836dbb9
UkiDLucas <UkiDLucas@gmail.com>
2020-12-06 11:41:26 -0500

    updated UBER with a semi-automatic processing

--- src/Julia/Uber_DataFrame.jl
+++ src/Julia/Uber_DataFrame.jl
@@ -1,154 +1,121 @@
 dir="../Data/processed/"
-
 dataset_file_name = "uber.csv"
 
-file1 = "^DJI.csv"
-file2 = "united-states.ism-manufacturing-employment.csv"
-file3 = "united-states.housing-starts-mm.csv"
-file4 = "united-states.ism-manufacturing-pmi.csv"
-file5 = "^VIX.csv"
-
-
 include("../Julia/functions.jl") 
+include("../Julia/IndicatorData.jl") 
+include("../Julia/function_format_rata_die_to_us_date.jl")
+include("../Julia/function_trim_DataFrames.jl")
 println()
 
-df1 = fetch_dataset( file1 , "yyyy-mm-dd", dir )
+data = Dict{String, IndicatorData}()
+
+## Do NOT use "^" in names as in "^VIX"
+addIndicator!(data, "AAPL", "AAPL.csv" )
+addIndicator!(data, "VIX", "^VIX.csv" )
+addIndicator!(data, "DJIA", "^DJI.csv" )
+addIndicator!(data, "US_ISM_MFC_EMP", "united-states.ism-manufacturing-employment.csv" )
+addIndicator!(data, "US_HOUS_SRTS_M", "united-states.housing-starts-mm.csv" )
+addIndicator!(data, "US_ISM_MFC_PMI", "united-states.ism-manufacturing-pmi.csv" )
+
+print()
+
+data
+
+data["AAPL"].min_rata_die
 
 using Statistics
-describe(df1)
+describe(data["AAPL"].df)
 
-df2 = fetch_dataset( file2 , "yyyy-mm-dd", dir )
+trim_DataFrames!(data) # include("../Julia/function_trim_DataFrames.jl")
 
-using Statistics
-describe(df2)
+## Print POSSIBLE fields for UBER DataFrame
 
-df3 = fetch_dataset( file3 , "yyyy-mm-dd", dir )
-
-using Statistics
-describe(df3)
-
-df4 = fetch_dataset( file4 , "yyyy-mm-dd", dir )
-
-using Statistics
-describe(df3)
-
-df5 = fetch_dataset( file5 , "yyyy-mm-dd", dir )
-
-using Statistics
-describe(df3)
-
-using Dates
-
-function format_rata_die_to_us_date(d)
-    return Dates.format( rata2datetime(d) , "u. d, yyyy")
+for key in keys(data) 
+    #df = data[key].df
+    columns = names(data[key].df)
+    for i in 1:length(columns)
+        println(",", data[key].name, "_",columns[i], " = data[\"", data[key].name, "\"].df[:,:", columns[i], "] ") 
+    end
 end
-
-oldest_common_day = maximum([
-         minimum(df1[:,1]) 
-        ,minimum(df2[:,1]) 
-        ,minimum(df3[:,1])
-        ,minimum(df4[:,1])
-        ,minimum(df5[:,1])
-        ])
-
-
-println("Oldest common day for df1 is ", format_rata_die_to_us_date( minimum(df1[:,1]))  )
-println("Oldest common day for df2 is ", format_rata_die_to_us_date( minimum(df2[:,1]))  )
-println("Oldest common day for df3 is ", format_rata_die_to_us_date( minimum(df3[:,1]))  )
-println("Oldest common day for df4 is ", format_rata_die_to_us_date( minimum(df4[:,1]))  )
-println("Oldest common day for df5 is ", format_rata_die_to_us_date( minimum(df5[:,1]))  )
-
-println("Oldest common day is ", format_rata_die_to_us_date(oldest_common_day) )
-
-newest_common_day = minimum([
-          maximum(df1[:,1])
-        , maximum(df2[:,1])
-        , maximum(df3[:,1])
-        , maximum(df4[:,1])
-        , maximum(df5[:,1])
-        ])
-
-println("Newest common day for df1 is ", format_rata_die_to_us_date( maximum(df1[:,1]))  )
-println("Newest common day for df2 is ", format_rata_die_to_us_date( maximum(df2[:,1]))  )
-println("Newest common day for df3 is ", format_rata_die_to_us_date( maximum(df3[:,1]))  )
-println("Newest common day for df4 is ", format_rata_die_to_us_date( maximum(df4[:,1]))  )
-println("Newest common day for df5 is ", format_rata_die_to_us_date( maximum(df5[:,1]))  )
-
-println("Newest common day is ", format_rata_die_to_us_date(newest_common_day) )
-
-function trim_DataFrame(name::String, df::DataFrame, oldest_common_day::Int64,  newest_common_day::Int64)
-    oldest_row = find_day(df, oldest_common_day)
-    newest_row = find_day(df, newest_common_day)
-    df = df[oldest_row:newest_row,:] # trimming
-    println(name, " ", size(df)[1],"\n    ", names(df) )
-    return df
-end
-
-df1 = trim_DataFrame("^DJI", df1, oldest_common_day, newest_common_day)
-df2 = trim_DataFrame("mnfc_epmlt", df2, oldest_common_day, newest_common_day)
-df3 = trim_DataFrame("hous_start", df3, oldest_common_day, newest_common_day)
-df4 = trim_DataFrame("mnfc_PMI", df4, oldest_common_day, newest_common_day)
-df5 = trim_DataFrame("^VIX", df5, oldest_common_day, newest_common_day)
-println()
 
 using DataFrames
-uber_df = DataFrame(  Rata_Die              = df1[:,:Rata_Die] 
-                     ,Date                  = df1[:,:Date] 
-    
-                     ,DJIA_Original         = df1[:,:Original] 
-                     ,DJIA_Quantized        = df1[:,:Quantized] 
-                     ,DJIA_Avg030           = df1[:,:Avg030] 
-                     ,DJIA_Avg060           = df1[:,:Avg060] 
-                     ,DJIA_Avg090           = df1[:,:Avg090] 
-                     ,DJIA_Avg120           = df1[:,:Avg120] 
-                     ,DJIA_Avg180          = df1[:,:Avg180] 
-                     ,DJIA_Avg365          = df1[:,:Avg365] 
-    
-                     ,mnfc_epmlt_Original   = df2[:,:Original] 
-                     ,mnfc_epmlt_Quantized  = df2[:,:Quantized]
-                     ,mnfc_epmlt_Avg030     = df2[:,:Avg030]
-                     ,mnfc_epmlt_Avg060     = df2[:,:Avg060]
-                     ,mnfc_epmlt_Avg090     = df2[:,:Avg090] 
-                     ,mnfc_epmlt_Avg120     = df1[:,:Avg120] 
-                     ,mnfc_epmlt_Avg180     = df1[:,:Avg180]
-                     ,mnfc_epmlt_Avg365    = df1[:,:Avg365] 
-    
-                     ,mnfc_PMI_Original     = df4[:,:Original] 
-                     ,mnfc_PMI_Quantized    = df4[:,:Quantized] 
-                     ,mnfc_PMI_Avg030       = df4[:,:Avg030]  
-                     ,mnfc_PMI_Avg060       = df4[:,:Avg060]  
-                     ,mnfc_PMI_Avg090       = df4[:,:Avg090] 
-                    #,mnfc_PMI_Avg120       = df1[:,:Avg120] 
-                     ,mnfc_PMI_Avg180       = df1[:,:Avg180] 
-                     ,mnfc_PMI_Avg365       = df1[:,:Avg365] 
-    
-                     ,VIX_Original          = df5[:,:Original] 
-                     ,VIX_Quantized         = df5[:,:Quantized] 
-                     ,VIX_Avg030            = df5[:,:Avg030]  
-                     ,VIX_Avg060            = df5[:,:Avg060]  
-                     ,VIX_Avg090            = df5[:,:Avg090] 
-                     ,VIX_Avg120            = df1[:,:Avg120] 
-                     ,VIX_Avg180            = df1[:,:Avg180] 
-                     ,VIX_Avg365            = df1[:,:Avg365] 
+uber = DataFrame(  
 
-                   )
+ Rata_Die = data["DJIA"].df[:,:Rata_Die] # Array{Int64,1}
+,Date = data["DJIA"].df[:,:Date] 
+    
+,VIX_Original = data["VIX"].df[:,:Original] 
+,VIX_Quantized = data["VIX"].df[:,:Quantized] 
+,VIX_Avg030 = data["VIX"].df[:,:Avg030] 
+,VIX_Avg060 = data["VIX"].df[:,:Avg060] 
+,VIX_Avg090 = data["VIX"].df[:,:Avg090] 
+,VIX_Avg120 = data["VIX"].df[:,:Avg120] 
+,VIX_Avg180 = data["VIX"].df[:,:Avg180] 
+,VIX_Avg365 = data["VIX"].df[:,:Avg365] 
+    
+,US_ISM_MFC_PMI_Original = data["US_ISM_MFC_PMI"].df[:,:Original] 
+,US_ISM_MFC_PMI_Quantized = data["US_ISM_MFC_PMI"].df[:,:Quantized] 
+,US_ISM_MFC_PMI_Avg030 = data["US_ISM_MFC_PMI"].df[:,:Avg030] 
+,US_ISM_MFC_PMI_Avg060 = data["US_ISM_MFC_PMI"].df[:,:Avg060] 
+,US_ISM_MFC_PMI_Avg090 = data["US_ISM_MFC_PMI"].df[:,:Avg090] 
+,US_ISM_MFC_PMI_Avg180 = data["US_ISM_MFC_PMI"].df[:,:Avg180] 
+,US_ISM_MFC_PMI_Avg365 = data["US_ISM_MFC_PMI"].df[:,:Avg365] 
+
+,DJIA_Original = data["DJIA"].df[:,:Original] 
+,DJIA_Quantized = data["DJIA"].df[:,:Quantized] 
+,DJIA_Avg030 = data["DJIA"].df[:,:Avg030] 
+,DJIA_Avg060 = data["DJIA"].df[:,:Avg060] 
+,DJIA_Avg090 = data["DJIA"].df[:,:Avg090] 
+,DJIA_Avg120 = data["DJIA"].df[:,:Avg120] 
+,DJIA_Avg180 = data["DJIA"].df[:,:Avg180] 
+,DJIA_Avg365 = data["DJIA"].df[:,:Avg365] 
+
+,AAPL_Original = data["AAPL"].df[:,:Original] 
+,AAPL_Quantized = data["AAPL"].df[:,:Quantized] 
+,AAPL_Avg030 = data["AAPL"].df[:,:Avg030] 
+,AAPL_Avg060 = data["AAPL"].df[:,:Avg060] 
+,AAPL_Avg090 = data["AAPL"].df[:,:Avg090] 
+,AAPL_Avg120 = data["AAPL"].df[:,:Avg120] 
+,AAPL_Avg180 = data["AAPL"].df[:,:Avg180] 
+
+,US_ISM_MFC_EMP_Original = data["US_ISM_MFC_EMP"].df[:,:Original] 
+,US_ISM_MFC_EMP_Quantized = data["US_ISM_MFC_EMP"].df[:,:Quantized] 
+,US_ISM_MFC_EMP_Avg030 = data["US_ISM_MFC_EMP"].df[:,:Avg030] 
+,US_ISM_MFC_EMP_Avg060 = data["US_ISM_MFC_EMP"].df[:,:Avg060] 
+,US_ISM_MFC_EMP_Avg090 = data["US_ISM_MFC_EMP"].df[:,:Avg090] 
+,US_ISM_MFC_EMP_Avg120 = data["US_ISM_MFC_EMP"].df[:,:Avg120] 
+,US_ISM_MFC_EMP_Avg180 = data["US_ISM_MFC_EMP"].df[:,:Avg180] 
+,US_ISM_MFC_EMP_Avg365 = data["US_ISM_MFC_EMP"].df[:,:Avg365] 
+
+#,US_HOUS_SRTS_M_Original = data["US_HOUS_SRTS_M"].df[:,:Original] 
+#,US_HOUS_SRTS_M_Quantized = data["US_HOUS_SRTS_M"].df[:,:Quantized] 
+#,US_HOUS_SRTS_M_Avg030 = data["US_HOUS_SRTS_M"].df[:,:Avg030] 
+#,US_HOUS_SRTS_M_Avg060 = data["US_HOUS_SRTS_M"].df[:,:Avg060] 
+#,US_HOUS_SRTS_M_Avg090 = data["US_HOUS_SRTS_M"].df[:,:Avg090] 
+#,US_HOUS_SRTS_M_Avg180 = data["US_HOUS_SRTS_M"].df[:,:Avg180] 
+#,US_HOUS_SRTS_M_Avg365 = data["US_HOUS_SRTS_M"].df[:,:Avg365] 
+)
 using Statistics
 describe(uber_df)
 
-columns = print_colunms(uber_df)
+columns = print_colunms(uber)
 println()
 
 using Plots
 
-record_count = size(uber_df)[1]
+record_count = size(uber)[1]
 rows = 1:record_count
-dates = format_dates( uber_df[rows,2] , "m/d/yy")
+dates = format_dates( uber[rows,2] , "m/d/yy")
 
 gr()
-plot(          dates, # x-axis: dates
-               [ uber_df[rows,6] uber_df[rows,14] uber_df[rows,22] uber_df[rows,29]         ], # y-axis
-    label    = [      columns[6]      columns[14]      columns[22]      columns[29]      "" ],
-    legend   =:bottom, 
+plot( dates, # x-axis: dates
+     [ 
+        uber[rows,5] uber[rows,8] uber[rows,14] uber[rows,16] uber[rows,20] uber[rows,23] uber[rows,28] uber[rows,31] uber[rows,36] uber[rows,38]         
+    ], # y-axis
+    label = [      
+          columns[5]   columns[8]   columns[14]   columns[16]   columns[20]    columns[23]  columns[28]    columns[31]  columns[36]   columns[38] "" 
+    ],
+    legend   =:topleft, 
               # :right, :left, :top, :bottom, :inside, :best, :legend, :topright, :topleft, :bottomleft, :bottomright
     xlabel   = "time",
     ylabel   = "indicators",
@@ -156,12 +123,16 @@ plot(          dates, # x-axis: dates
     layout = (1, 1) # number of graphs: vertically, horizontally
     )
 
-record_count = size(uber_df)[1]
-today_rata = Dates.datetime2rata( today() )
-today_id = find_day(uber_df, today_rata)
+savefig("../../indicators.png")
 
-df_training   = uber_df[1:today_id,:]
-df_prediction = uber_df[today_id-1:record_count,:]
+record_count = size(uber)[1]
+today_rata = Dates.datetime2rata( today() )
+today_id = find_day(uber, today_rata)
+
+# I am putting overlap in the sets to see the behavior
+df_training   = uber[1:today_id-3,:] # train up 3 days ago
+df_prediction = uber[today_id-60:record_count,:] # show predictions up to 60 days ago
+
 println()
 
 save_dataset(df_training,   "uber_training.csv"  , "../Data/processed/");
