d54914d030486ffa67265e70eabf5a69295a9e22
UkiDLucas <UkiDLucas@gmail.com>
2020-12-10 20:00:39 -0500

    fetching and preprocessing CSV from spreadsheet

--- src/Julia/Main.jl
+++ src/Julia/Main.jl
@@ -5,43 +5,22 @@ original_data_directory = "../DATA/original/" # MacOS
 #include("MarketIndicators.jl")
 include("function_update_yahoo_finance.jl")
 include("function_fetch_web_data.jl")
-
-using CSV, DataFrames, Dates
-file_path = "../DATA/_DataSets.csv"
-date_format="mm/dd/yyyy" # 9/14/2020
-df = CSV.read( file_path, DataFrame, dateformat="$date_format" )
-df[:, [1,3] ]
+include("preprocess_csv.jl")
+include("functions.jl") 
+include("function_toFloat64.jl")
 
 function print_date_time()
     println("============== ", Dates.DateTime(Dates.now() ), " ==============")
 end # function
 
+print_date_time();
+
 while true
-    # print_date_time(); fetch_web_data(df ); 
-    print_date_time(); include("preprocess_DJI.jl")
-    print_date_time(); include("preprocess_VIX.jl")
-    print_date_time(); include("preprocess_AAPL.jl")
-    print_date_time(); include("preprocess_NVDA.jl") 
-    print_date_time(); include("preprocess_NIO.jl") # fix Rata_Die
-    print_date_time(); include("preprocess_US_GDP_Q.jl")  
-    print_date_time(); include("US_HOUS_STRT_M.jl")
-    print_date_time(); include("ISM_Manufacturing_PMI.jl")
-    print_date_time(); include("ISM_Manufacturing_Employment.jl")
-    print_date_time(); include("US_INIT_JOBLESS.jl")  
-    print_date_time(); include("US_PEND_HOME_SALE.jl") 
+    fetch_web_data( "../DATA/Indicators.csv", original_data_directory ); 
+    #include("preprocess_DJI.jl"); print_date_time(); 
+    #include("Uber_DataFrame.jl"); print_date_time(); 
+    #include("predict_NVDA.jl"); print_date_time(); 
     
-    print_date_time(); include("Uber_DataFrame.jl")
-
-    print_date_time(); include("predict_NVDA.jl")
-    print_date_time(); include("predict_VIX.jl")
-    print_date_time(); include("predict_AAPL.jl")
-    print_date_time(); include("predict_NIO.jl")
-   
-    print_date_time(); include("preditct_DJIA.jl")
-    
-    
-    
-
     nap_time = 60*60*4 # seconds * minutes * hours
     print_date_time(); println("going to sleep for ", nap_time/(60*60), " hours" )
     sleep(nap_time) 
--- src/Julia/function_fetch_web_data.jl
+++ src/Julia/function_fetch_web_data.jl
@@ -1,21 +1,35 @@
 using CSV, DataFrames, Dates
 
-function fetch_web_data(df::DataFrame)
+function fetch_web_data(file_path::String, original_data_directory)
+
+    date_format="mm/dd/yyyy" # 9/14/2020
+    df = CSV.read( file_path, DataFrame, dateformat="$date_format" )
     row_count = size(df)[1]
 
     for row in 1:row_count
-        indicatior_name  = df[row, 1]
-        file_name        = df[row, 2]
-        url              = df[row, 3]
+        indicator_full_name  = df[row, :Indicator_Full_Name]
+        url                  = df[row, :URL]
+        symbol               = df[row, :Symbol]
+        original_date_format = df[row, :Original_Date_Format]
+        column_to_keep       = df[row, :Column_To_Keep]
+        
+        ## update date for Yahoo Finance URL
         if !isnothing(findfirst("finance.yahoo.com", url))
             url = update_yahoo_finance(url)
         end
-        download( url , string(original_data_directory * file_name ) )
-        println( "- downloaded: ", indicatior_name, " 
-            -- ", file_name, "
+
+        download( url , string(original_data_directory * symbol * ".csv" ) )
+        println( "- downloaded: ", symbol, " -- ", indicator_full_name, " 
             -- ", url ) 
+
+        ## preprocess 
+        preprocess_csv(symbol, original_date_format, column_to_keep)
+
+
+        # Slow down with random interval
         r_array = (rand(1)*9).+1  # 1.0.. to 9.9.. seconds
         r = r_array[1]
-        sleep(r)            # to prevent being blocked
+        #sleep(r)            # to prevent being blocked
     end # for loop
+    df = nothing # free the memory for gc()
 end  # function
