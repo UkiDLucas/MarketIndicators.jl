881dce5e6a187187f7d2afc7a47e3ee39601d1ec
UkiDLucas <UkiDLucas@gmail.com>
2020-12-14 17:36:41 -0500

    re-run

--- src/Julia/Main.jl
+++ src/Julia/Main.jl
@@ -21,17 +21,17 @@ end # function
 print_date_time();
 
 while true
-    fetch_web_data( "../DATA/Indicators.csv", original_data_directory ); 
+    #fetch_web_data( "../DATA/Indicators.csv", original_data_directory ); 
     include("Uber_DataFrame.jl"); print_date_time(); 
 
-    include("predict_DJIA.jl"); print_date_time(); 
     include("predict_AAPL.jl"); print_date_time(); 
-    include("predict_VIX.jl"); print_date_time(); 
+    include("predict_BABA.jl"); print_date_time(); 
+    include("predict_CRSP.jl"); print_date_time(); 
+    include("predict_DJIA.jl"); print_date_time(); 
     include("predict_NVDA.jl"); print_date_time(); 
     include("predict_TPLGX.jl"); print_date_time(); 
-    include("predict_CRSP.jl"); print_date_time(); 
-    include("predict_TSLA.jl"); print_date_time(); 
-    include("predict_BABA.jl"); print_date_time(); 
+    include("predict_TSLA.jl"); print_date_time();
+    include("predict_VIX.jl"); print_date_time();  
     
     nap_time = 60*60*4 # seconds * minutes * hours
     print_date_time(); println("going to sleep for ", nap_time/(60*60), " hours" )
