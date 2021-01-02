1ceda095459f2644f44e16be406c6ad53ba92418
UkiDLucas <UkiDLucas@gmail.com>
2021-01-02 10:38:31 -0500

    cleanup

--- src/Julia/trim_DataFrames.jl
+++ src/Julia/trim_DataFrames.jl
@@ -1,11 +1,13 @@
 #include("../Julia/IndicatorData.jl") # addIndicator!()
 
+using Dates
+
 function trim_DataFrames!(data::Dict{String, IndicatorData.Indicator})
     oldest = get_oldest_common_day(data)
-    println("oldest = ", oldest, " ", typeof(oldest))
+    println("oldest = ", oldest, " ", typeof(oldest), " ", Dates.rata2datetime(oldest) )
 
     newest = get_newest_common_day(data)
-    println("newest = ", newest, " ", typeof(oldest))
+    println("newest = ", newest, " ", typeof(newest), " ", Dates.rata2datetime(newest) )
 
     
     ## Iterate thru each Indicator
@@ -27,19 +29,22 @@ function get_newest_common_day(data::Dict{String, IndicatorData.Indicator})
     v = Int64[]
 
     for key in keys(data) 
-        println(" - ", data[key].name, " max_rata_die = ", data[key].max_rata_die ) 
-        push!(v, data[key].max_rata_die)
+        local rata_die = data[key].max_rata_die
+        ## The all are the 7 (or whatever was set) days from today
+        #println(" - ", Dates.rata2datetime(rata_die), " \t max_rata_die = ", rata_die, " \t", data[key].name  )
+        push!(v, rata_die)
     end
-    return minimum( v)
+    return minimum( v )
 end
 
-
+## Iterate thru all Indicators and find the biggest in minium Rata Die
 function get_oldest_common_day(data::Dict{String, IndicatorData.Indicator})
     v = Int64[]
 
     for key in keys(data) 
-        println(" - ", data[key].name, " min_rata_die = ", data[key].min_rata_die ) 
-        push!(v, data[key].min_rata_die)
+        local rata_die = data[key].min_rata_die
+        println(" - ", Dates.rata2datetime(rata_die), " \t min_rata_die = ", rata_die, " \t", data[key].name  )
+        push!(v, rata_die)
     end
-    return maximum( v)
+    return maximum( v )
 end
